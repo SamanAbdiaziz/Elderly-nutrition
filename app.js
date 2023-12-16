a02bc42ceec30204de2e5e653d7a35f2679d40dc
Abdiaziz <abdiazizsaman394@gmail.com>
2023-12-17 00:59:47 +0300

    Changes made

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 
-var staySignedFlag = localStorage.getItem("staySI");
-var loginClick = document.getElementById("navLogin-btn");
-loginClick.addEventListener("click",function(){
-    if(staySignedFlag == "true"){
-        //open profile page directly
-        loginClick.setAttribute("href","index.php");
-        window.location.replace("index.php");
-    }
-    else{
-        //open login page
-        loginClick.setAttribute("href","login.php");
-        window.location.replace("login.php");
-    }
+// var staySignedFlag = localStorage.getItem("staySI");
+// var loginClick = document.getElementById("navLogin-btn");
+// loginClick.addEventListener("click",function(){
+//     if(staySignedFlag == "true"){
+//         //open profile page directly
+//         loginClick.setAttribute("href","index.php");
+//         window.location.replace("index.php");
+//     }
+//     else{
+//         //open login page
+//         loginClick.setAttribute("href","login.php");
+//         window.location.replace("login.php");
+//     }
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector("nav");
+
+    hamburger.addEventListener("click", function() {
+        nav.classList.toggle("active");
+    });
 });
 
 
